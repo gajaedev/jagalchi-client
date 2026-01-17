4ee862bb69cead79685bad6961bdd5a517a413d7
justn-hyeok <justn.hyeok@gmail.com>
2026-01-17 17:34:49 +0900

    Revert "feat(#42): React Flow custom node components (#52)"
    This reverts commit 5e1e7229f3ba9abe2ad7b7e0271120ab466095e3.

--- src/features/editor/components/index.ts
+++ src/features/editor/components/index.ts
@@ -1,5 +1,5 @@
-export { AIMenu, ResourceInput, ToolbarDropdown, ToolbarItem } from './atoms';
-export { EditorHeader, EditorToolbar, ResourceDisplay } from './molecules';
+export { ResourceInput, ToolbarDropdown, ToolbarItem } from './atoms';
+export { EditorHeader, EditorToolbar } from './molecules';
 export {
   LineSidebar,
   MultiSelectionSidebar,
@@ -7,4 +7,3 @@ export {
   SectionSidebar,
   TextSidebar,
 } from './organisms';
-export { FlowNode, FlowSection, FlowText } from './flow-nodes';
